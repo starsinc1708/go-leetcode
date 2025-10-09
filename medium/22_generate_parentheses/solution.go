1ba4752438707cf998867c051d1aa7234348abea
starsinc1708 <starsinc12@yandex.ru>
2025-10-09 14:18:53 +0400

    add https://leetcode.com/problems/generate-parentheses solution

---
+++ medium/22_generate_parentheses/solution.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/generate-parentheses
+// Time complexity: O(4^N/Sqrt(N))
+func generateParenthesis(n int) []string {
+	var ans []string
+	backTrack(&ans, "", 0, 0, n)
+	return ans
+}
+
+func backTrack(result *[]string, currStr string, openCount int, closeCount int, n int) {
+	if len(currStr) == n*2 {
+		*result = append(*result, currStr)
+		return
+	}
+
+	if openCount < n {
+		backTrack(result, currStr+"(", openCount+1, closeCount, n)
+	}
+	if closeCount < openCount {
+		backTrack(result, currStr+")", openCount, closeCount+1, n)
+	}
+
+}
+
+func main() {
+	fmt.Println(generateParenthesis(3))
+}
