19185612599fd5850a95f709c6befdff80e0b5be
starsinc1708 <starsinc12@yandex.ru>
2025-12-12 13:02:35 +0400

    sliding window solutions

---
+++ easy/643_max_average_subarray/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func findMaxAverage(nums []int, k int) float64 {
+	begin := 0
+	windowState := float64(0)
+	result := math.Inf(-1)
+	for end := range nums {
+		windowState += float64(nums[end])
+		if end-begin+1 == k {
+			result = math.Max(result, windowState)
+			windowState -= float64(nums[begin])
+			begin++
+		}
+	}
+
+	return result / float64(k)
+}
+
+func main() {
+	fmt.Println(findMaxAverage([]int{1, 2, 3, 4, 5}, 2))
+}
---
+++ medium/209_min_size_subarray_sum/solution.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func minSubArrayLen(target int, nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	begin := 0
+	windowState := 0
+	result := math.MaxInt64
+
+	for end := 0; end < len(nums); end++ {
+		windowState += nums[end]
+
+		for windowState >= target {
+			windowSize := end - begin + 1
+			result = min(result, windowSize)
+			windowState -= nums[begin]
+			begin++
+		}
+	}
+
+	if result == math.MaxInt64 {
+		return 0
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3}))  // 2
+	fmt.Println(minSubArrayLen(11, []int{2, 3, 1, 2, 4, 3})) // 5
+	fmt.Println(minSubArrayLen(11, []int{1, 1, 1}))          // 0
+	fmt.Println(minSubArrayLen(4, []int{1, 4, 4}))           // 1
+
+}
